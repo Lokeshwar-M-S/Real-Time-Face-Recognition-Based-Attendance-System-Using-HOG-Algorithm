688df4d27a073e94f471f0b6b0bd04c17519b700
Lokeshwar <88943696+Lokeshwar-M-S@users.noreply.github.com>
2023-07-03 19:24:05 +0530

    Add files via upload

---
+++ Project Coding/face_recognition.py
@@ -0,0 +1,156 @@
+# import re
+import csv
+from sys import path
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+import os
+import mysql.connector
+import cv2
+import numpy as np
+from tkinter import messagebox
+from time import strftime
+from datetime import datetime
+class Face_Recognition:
+
+    def __init__(self,root):
+
+        self.root=root
+        self.root.geometry("1366x768+0+0")
+        self.root.title("Face Recognition Pannel")
+
+        # This part is image labels setting start 
+        # first header image  
+        
+        img=Image.open(r"Images_GUI\banner.jpg")
+        img=img.resize((1366,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        # set image as lable
+        f_lb1 = Label(self.root,image=self.photoimg)
+        f_lb1.place(x=0,y=0,width=1366,height=130)
+
+        # backgorund image 
+        bg1=Image.open(r"Images_GUI\bg2.jpg")
+        bg1=bg1.resize((1366,768),Image.ANTIALIAS)
+        self.photobg1=ImageTk.PhotoImage(bg1)
+
+        # set image as lable
+        bg_img = Label(self.root,image=self.photobg1)
+        bg_img.place(x=0,y=130,width=1366,height=768)
+
+
+        #title section
+        title_lb1 = Label(bg_img,text="Welcome to Face Recognition Pannel",font=("verdana",30,"bold"),bg="white",fg="navyblue")
+        title_lb1.place(x=0,y=0,width=1366,height=45)
+
+        # Create buttons below the section 
+        # ------------------------------------------------------------------------------------------------------------------- 
+        # Training button 1
+        std_img_btn=Image.open(r"Images_GUI\f_det.jpg")
+        std_img_btn=std_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.std_img1=ImageTk.PhotoImage(std_img_btn)
+
+        std_b1 = Button(bg_img,command=self.face_recog,image=self.std_img1,cursor="hand2")
+        std_b1.place(x=600,y=170,width=180,height=180)
+
+        std_b1_1 = Button(bg_img,command=self.face_recog,text="Face Detector",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        std_b1_1.place(x=600,y=350,width=180,height=45)
+    #=====================Attendance===================
+
+    def mark_attendance(self, i, r, n, d):
+        lines = []
+        with open("attendance.csv", "r") as f:
+            csv_reader = csv.reader(f)
+            lines = list(csv_reader)
+    
+        for line in lines:
+            if line[0] == i:
+                line[-1] = "Present"
+                line[-2] = datetime.now().strftime("%d/%m/%Y")
+                line[-3] = datetime.now().strftime("%H:%M:%S")
+                break
+    
+        with open("attendance.csv", "w", newline="") as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerows(lines)
+
+    #================face recognition==================
+    def face_recog(self):
+    
+        def draw_boundray(img,classifier,scaleFactor,minNeighbors,color,text,clf):
+            global d
+            gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            featuers=classifier.detectMultiScale(gray_image,scaleFactor,minNeighbors)
+
+            coord=[]
+            
+            for (x,y,w,h) in featuers:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+                id,predict=clf.predict(gray_image[y:y+h,x:x+w])
+
+                confidence=int((100*(1-predict/300)))
+
+                conn = mysql.connector.connect(user='root', password='',host='localhost',database='face_recognition',port=3306)
+                cursor = conn.cursor()
+
+                cursor.execute("select Name from student where Student_ID="+str(id))
+                n=cursor.fetchone()
+                n="+".join(n)
+
+                cursor.execute("select Roll_No from student where Student_ID="+str(id))
+                r=cursor.fetchone()
+                r="+".join(r)
+
+                cursor.execute("select Department from student where Student_ID="+str(id))
+                d=cursor.fetchone()
+                d="+".join(d)
+
+                cursor.execute("select Student_ID from student where Student_ID="+str(id))
+                i=cursor.fetchone()
+                i="+".join(i)
+
+
+                if confidence > 77:
+                    cv2.putText(img,f"Student_ID:{i}",(x,y-80),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
+                    cv2.putText(img,f"Name:{n}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
+                    cv2.putText(img,f"Roll-No:{r}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
+                    cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
+                    self.mark_attendance(i,r,n,d)
+                else:
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+                    cv2.putText(img,"Unknown Face",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,0),3)    
+
+                coord=[x,y,w,h]
+            
+            return coord    
+
+
+        #==========
+        def recognize(img,clf,faceCascade):
+            coord=draw_boundray(img,faceCascade,1.1,10,(255,25,255),"Face",clf)
+            return img
+        
+        faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.read("clf.xml")
+
+        videoCap=cv2.VideoCapture(0)
+
+        while True:
+            ret,img=videoCap.read()
+            img=recognize(img,clf,faceCascade)
+            cv2.imshow("Face Detector",img)
+
+            if cv2.waitKey(1) == 13:
+                break
+        videoCap.release()
+        cv2.destroyAllWindows()
+
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Face_Recognition(root)
+    root.mainloop()
